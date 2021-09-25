6d22ef0e5c90319a9db7b9eaadf823ab9eb8bd03
rwxrob <rob@rwx.gg>
2021-09-25 19:12:53 -0400

    Add initial package doc.go

---
+++ doc.go
@@ -0,0 +1,5 @@
+/* Package conf blah blah
+
+The values file format is 100% compatible with the Java properties specification enabling other parsers to be used when necessary. The format is, however, a very limited subset of the full Java properties specification.
+*/
+package conf
